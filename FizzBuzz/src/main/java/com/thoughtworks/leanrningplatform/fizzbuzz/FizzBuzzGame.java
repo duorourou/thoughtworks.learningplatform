0d444ff3c8b69a242c690eecc1b0f184c9e95622
duorourou <duoorourou@gmail.com>
2020-01-10 17:05:59 +0800

    [Xiaocheng HOU] #000 number off 3

---
+++ FizzBuzz/src/main/java/com/thoughtworks/leanrningplatform/fizzbuzz/FizzBuzzGame.java
@@ -0,0 +1,18 @@
+package com.thoughtworks.leanrningplatform.fizzbuzz;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class FizzBuzzGame {
+
+
+    public String numberOff(Integer integer) {
+        return "Fizz";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ FizzBuzz/src/test/groovy/com/thoughtworks/leanrningplatform/fizzbuzz/FizzBuzzGameTest.groovy
@@ -0,0 +1,29 @@
+package com.thoughtworks.leanrningplatform.fizzbuzz
+
+import spock.lang.Specification
+
+class FizzBuzzGameTest extends Specification {
+
+  def fizzBuzzGame = new FizzBuzzGame();
+
+  def "it will return fizz"() {
+    given: "the student's number"
+      def studentNumber = 3
+    when:
+      def result = fizzBuzzGame.numberOff(studentNumber)
+    then:
+      result == "Fizz"
+  }
+
+}
+
+
+/**
+ 让所有学生拍成一队，然后按顺序报数。
+ 学生报数时，如果所报数字是3的倍数，那么不能说该数字，而要说Fizz；如果所报数字是5的倍数，那么要说Buzz；如果所报数字是7的倍数，那么要说Whizz。
+ 学生报数时，如果所报数字同时是两个特殊数的倍数情况下，也要特殊处理，比如3和5的倍数，那么不能说该数字，而是要说FizzBuzz, 以此类推。如果同时是三个特殊数的倍数，那么要说FizzBuzzWhizz。
+ 学生报数时，如果所报数字包含了3，那么也不能说该数字，而是要说相应的单词，比如要报13的同学应该说Fizz。
+ 如果数字中包含了3，那么忽略规则2和规则3，比如要报30的同学只报Fizz，不报FizzBuzz。
+ 如果数字中包含了5，那么忽略规则4和规则5，并且忽略被3整除的判定，比如要报35的同学不报Fizz，报BuzzWhizz。
+ 如果数字中包含了7，那么忽略规则6，并且忽略被5整除的判定，比如要报75的同学只报FIzz，其他case自己补齐。
+ */
